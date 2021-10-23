5284b9b450bbae5cdbca83f8a98d5c476ac9f7ef
ArdenyUser <71571434+ArdenyUser@users.noreply.github.com>
2021-10-23 11:02:11 -0400

    Update GPACKERtest.ps1

--- GPACKERtest.ps1
+++ GPACKERtest.ps1
@@ -1,4 +1,3 @@
-Install-Module ps2exe
 $Gfile = Read-Host "GPACKER's Unpacker, please enter a filename, no extension: "
 $UnpackLocation = Read-Host "Directory to unpack in: "
 $Gfile = $Gfile + ".gpacker"
@@ -7,20 +6,21 @@ $Gfile = $Gfile + ".gpacker"
 "cd " + $UnpackLocation | Out-File -FilePath C:\gpacker.ps1 -Append
 $file_data_listdownloader = Get-Content "gpackerlist"
 "wget " + $file_data_listdownloader[0] | Out-File -FilePath C:\gpacker.ps1 -Append
-"$file_data_listdownloader = Get-Content 'gackerlist'" | Out-File -FilePath C:\gpacker.ps1 -Append
+"$file_data_listdownloader = Get-Content 'gpackerlist'" | Out-File -FilePath C:\gpacker.ps1 -Append
+"$lineno = 0" | Out-File -FilePath C:\gpacker.ps1 -Append
+"DO" | Out-File -FilePath C:\gpacker.ps1 -Append
+"{" | Out-File -FilePath C:\gpacker.ps1 -Append
+"if ($line -eq 'EXIT') {" | Out-File -FilePath C:\gpacker.ps1 -Append
+"echo 'End of list...'" | Out-File -FilePath C:\gpacker.ps1 -Append
+"}" | Out-File -FilePath C:\gpacker.ps1 -Append
+"else {" | Out-File -FilePath C:\gpacker.ps1 -Append
+"$dload = $file_data_listdownloader[" + $lineno + "]" + " | Out-File -FilePath C:\gpacker.ps1" | Out-File -FilePath C:\gpackerx.ps1 -Append
+"wget " + $dload | Out-File -FilePath C:\gpackerx.ps1 -Append
+"}" | Out-File -FilePath C:\gpacker.ps1 -Append
+"gpackerx.ps1" | Out-File -FilePath C:\gpackerx.ps1 -Append
+"$lineno = $lineno + 1" | Out-File -FilePath C:\gpacker.ps1 -Append
+"} Until ($line -eq 'EXIT')" | Out-File -FilePath C:\gpacker.ps1 -Append
 # PREPERATION
-$file_data_listdownloader = Get-Content "gpackerlist"
-$lineno = 0
-DO
-{
-if ($line -eq 'EXIT') {
-echo "End of list..."
-}
-else {
-$dload = $file_data_listdownloader[$lineno]
-}
-$lineno = $lineno + 1
-wget $dload
-} Until ($line -eq 'EXIT')
+
 
 
