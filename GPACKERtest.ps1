f45d092147cf61b4d132da561d92edfdc9539cc5
ArdenyUser <71571434+ArdenyUser@users.noreply.github.com>
2021-10-23 10:50:40 -0400

    Update GPACKERtest.ps1

--- GPACKERtest.ps1
+++ GPACKERtest.ps1
@@ -1,9 +1,14 @@
+Install-Module ps2exe
 $Gfile = Read-Host "GPACKER's Unpacker, please enter a filename, no extension: "
 $UnpackLocation = Read-Host "Directory to unpack in: "
 $Gfile = $Gfile + ".gpacker"
-$file_data_listdownloader = Get-Content $Gfile
-cd $UnpackLocation
-wget $file_data_listdownloader[0]
+# PREPERATION
+"$file_data_listdownloader = Get-Content " + $Gfile | Out-File -FilePath C:\gpacker.ps1
+"cd " + $UnpackLocation | Out-File -FilePath C:\gpacker.ps1 -Append
+$file_data_listdownloader = Get-Content "gpackerlist"
+"wget " + $file_data_listdownloader[0] | Out-File -FilePath C:\gpacker.ps1 -Append
+"$file_data_listdownloader = Get-Content 'gackerlist'" | Out-File -FilePath C:\gpacker.ps1 -Append
+# PREPERATION
 $file_data_listdownloader = Get-Content "gpackerlist"
 $lineno = 0
 DO
